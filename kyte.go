924b75d688faccd854cb78212dec1df1bc9d6673
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2024-01-15 22:28:15 +0300

    feat: add nullable source

--- kyte.go
+++ kyte.go
@@ -13,13 +13,15 @@ var (
 	ErrNilSource    = errors.New("source is nil")
 	ErrNotPtrSource = errors.New("source is not a pointer")
 
-	ErrEmptyField             = errors.New("field is empty")
+	ErrEmptyField             = errors.New("field is empty use string or pointer of an source struct field")
 	ErrNilPointerField        = errors.New("field is nil pointer")
 	ErrFieldMustBePtrOrString = errors.New("field must be string or pointer of an source struct field")
 
 	ErrNotValidFieldForQuery = errors.New("this field is not in the source struct")
 
-	ErrNilValue = errors.New("value is nil")
+	ErrNilValue          = errors.New("value is nil")
+	ErrNilField          = errors.New("field is nil")
+	ErrFieldMustBeString = errors.New("field must be string")
 )
 
 type Kyte struct {
@@ -27,23 +29,29 @@ type Kyte struct {
 	fields     map[any]string
 	fieldNames []string
 	errs       []error
-	fieldCheck bool
+	checkField bool
 }
 
-func newKyte(source any) *Kyte {
-	kyte := &Kyte{fields: make(map[any]string), fieldCheck: true}
+func newKyte(source any, checkField bool) *Kyte {
 	if source == nil {
-		kyte.errs = append(kyte.errs, ErrNilSource)
-		return kyte
+		return &Kyte{}
+	}
+	kyte := &Kyte{fields: make(map[any]string), checkField: checkField}
+	kyte.setSourceAndPrepareFields(source)
+	return kyte
+}
+
+func (k *Kyte) setSourceAndPrepareFields(source any) {
+	k.source = source
+	k.fields = make(map[any]string)
+	k.fieldNames = []string{}
+
+	if reflect.ValueOf(source).Kind() != reflect.Ptr {
+		k.errs = append(k.errs, ErrNotPtrSource)
+		return
 	}
 
-	k := reflect.ValueOf(source)
-	if k.Kind() != reflect.Ptr {
-		kyte.errs = append(kyte.errs, ErrNotPtrSource)
-		return kyte
-	}
-
-	kyte.source = source
+	k.source = source
 	v := reflect.ValueOf(source).Elem()
 	for i := 0; i < v.NumField(); i++ {
 		field := v.Type().Field(i)
@@ -54,19 +62,99 @@ func newKyte(source any) *Kyte {
 
 		bsonTag := getBsonTag(field)
 		if bsonTag != "" {
-			kyte.fields[fieldValue.Addr().Interface()] = bsonTag
+			k.fields[fieldValue.Addr().Interface()] = bsonTag
 		}
 
 		if field.Type.Kind() == reflect.Struct {
-			getSubStructFields(v.Field(i), bsonTag+".", kyte.fields)
+			getSubStructFields(v.Field(i), bsonTag+".", k.fields)
 		}
 	}
 
-	for _, v := range kyte.fields {
-		kyte.fieldNames = append(kyte.fieldNames, v)
+	for _, v := range k.fields {
+		k.fieldNames = append(k.fieldNames, v)
+	}
+}
+
+func (k *Kyte) Errors() []error {
+	return k.errs
+}
+
+func (k *Kyte) setError(err error) {
+	k.errs = append(k.errs, err)
+}
+
+func (k *Kyte) validateQueryFieldAndValue(field any, value any) (string, error) {
+	if len(k.errs) > 0 {
+		return "", k.errs[0]
 	}
 
-	return kyte
+	if value == nil {
+		return "", ErrNilValue
+	}
+
+	if field == nil {
+		return "", ErrNilField
+	}
+
+	fieldType := reflect.TypeOf(field)
+	if fieldType.Kind() != reflect.String && fieldType.Kind() != reflect.Ptr {
+		return "", ErrFieldMustBePtrOrString
+	}
+
+	if fieldType.Kind() == reflect.String && field.(string) == "" {
+		return "", ErrEmptyField
+	}
+
+	if fieldType.Kind() == reflect.Pointer && field == nil {
+		return "", ErrNilPointerField
+	}
+
+	if k.checkField && k.fields != nil {
+		fieldName := ""
+		if fieldType.Kind() == reflect.String {
+			fieldName = field.(string)
+		}
+
+		ok := false
+		if fieldType.Kind() == reflect.Ptr {
+			_, ok = k.fields[field]
+		}
+
+		if !ok && !contains(k.fieldNames, fieldName) {
+			return "", errors.Join(ErrNotValidFieldForQuery, errors.New(fmt.Sprintf("field: %s You can ignore this error by setting checkField to false", fieldName)))
+		}
+	}
+
+	fieldName, err := k.getFieldName(field)
+	if err != nil {
+		return "", err
+	}
+
+	return fieldName, nil
+}
+
+func (k *Kyte) hasErrors() bool {
+	return len(k.errs) > 0
+}
+
+func (k *Kyte) getFieldName(field any) (string, error) {
+	if reflect.TypeOf(field).Kind() == reflect.String {
+		return field.(string), nil
+	}
+
+	if reflect.TypeOf(field).Kind() == reflect.Ptr && k.fields == nil {
+		return "", ErrFieldMustBeString
+	}
+
+	if reflect.TypeOf(field).Kind() == reflect.Ptr {
+		fieldName, ok := k.fields[field]
+		if !ok {
+			return "", ErrNotValidFieldForQuery
+		}
+		return fieldName, nil
+	}
+
+	return "", ErrFieldMustBePtrOrString
 }
 
 func getBsonTag(field reflect.StructField) string {
@@ -113,79 +201,4 @@ func contains[T comparable](slice []T, item T) bool {
 		}
 	}
 	return false
-}
-
-func (k *Kyte) Errors() []error {
-	return k.errs
-}
-
-func (k *Kyte) setError(err error) {
-	k.errs = append(k.errs, err)
-}
-
-func (k *Kyte) validateQueryFieldAndValue(field any, value any) (string, error) {
-	if len(k.errs) > 0 {
-		return "", k.errs[0]
-	}
-
-	if value == nil {
-		return "", ErrNilValue
-	}
-
-	fieldType := reflect.TypeOf(field)
-	if fieldType.Kind() != reflect.String && fieldType.Kind() != reflect.Ptr {
-		return "", ErrFieldMustBePtrOrString
-	}
-
-	if fieldType.Kind() == reflect.String && field.(string) == "" {
-		return "", ErrEmptyField
-	}
-
-	if fieldType.Kind() == reflect.Pointer && field == nil {
-		return "", ErrNilPointerField
-	}
-
-	if k.fieldCheck {
-		fieldName := ""
-		if fieldType.Kind() == reflect.String {
-			fieldName = field.(string)
-		}
-
-		ok := false
-		if fieldType.Kind() == reflect.Ptr {
-			_, ok = k.fields[field]
-		}
-
-		if !ok && !contains(k.fieldNames, fieldName) {
-			return "", errors.Join(ErrNotValidFieldForQuery, errors.New(fmt.Sprintf("field: %s You can ignore this error by setting fieldCheck to false", fieldName)))
-		}
-
-	}
-
-	fieldName, err := k.getFieldName(field)
-	if err != nil {
-		return "", err
-	}
-
-	return fieldName, nil
-}
-
-func (k *Kyte) hasErrors() bool {
-	return len(k.errs) > 0
-}
-
-func (k *Kyte) getFieldName(field any) (string, error) {
-	if reflect.TypeOf(field).Kind() == reflect.String {
-		return field.(string), nil
-	}
-
-	if reflect.TypeOf(field).Kind() == reflect.Ptr {
-		fieldName, ok := k.fields[field]
-		if !ok {
-			return "", ErrNotValidFieldForQuery
-		}
-		return fieldName, nil
-	}
-
-	return "", ErrFieldMustBePtrOrString
 }
